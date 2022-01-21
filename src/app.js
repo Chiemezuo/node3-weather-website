eb333bbb6744904d4860fa76ee2b61ecad62268a
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-01-21 18:06:51 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geoCode = require('./utils/geocode')
 const forecast = require('./utils/weather')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -102,6 +103,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("A server is running")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
